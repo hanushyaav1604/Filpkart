06fdc4ac40e5b60404f4f999251a9202b814447b
hanushyaav1604 <hanushyaav1604@gmail.com>
2024-05-30 18:29:41 +0530

    Update HelloWorld.java

--- src/org/filpkart/HelloWorld.java
+++ src/org/filpkart/HelloWorld.java
@@ -3,7 +3,7 @@ package org.filpkart;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Test person 2");
 		System.out.println("Hello Filpkart v2");
 	}
 
